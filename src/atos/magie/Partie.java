0c050a6190202872dc1ca4d010dedad816855a33
Administrateur <Administrateur@GSVA.formation.local>
2018-06-14 11:39:54 +0200

    nos entitée sont mapées__ jai juste rajouté un espace

--- src/atos/magie/Partie.java
+++ src/atos/magie/Partie.java
@@ -21,7 +21,7 @@ import javax.persistence.OneToMany;
 @Entity
 public class Partie implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L; 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
